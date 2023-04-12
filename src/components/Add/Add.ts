caa41de405b24d71efc639d57d344cbe9a18a22c
vynayak <vynayak@synamedia.com>
2023-04-12 09:44:56 +0530

    initial commit

---
+++ src/components/Add/Add.ts
@@ -0,0 +1,12 @@
+import React from "react";
+import { Component } from "react";
+class Add extends Component
+{
+    render() {
+    let num1 = 30;
+    let num2 = 34;
+    return(num1+num2)}
+}
+export default Add;
+   
+
